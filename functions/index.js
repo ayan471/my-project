5ecc5e378b952ad786e791bcecd395325dd0a204
ayan471 <pakhiraayan@gmail.com>
2023-02-28 17:55:00 +0530

    started creating the create post

--- functions/index.js
+++ functions/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable indent */
 /* eslint-disable no-unused-vars */
 /* eslint-disable object-curly-spacing */
 const functions = require("firebase-functions");
@@ -7,8 +8,29 @@ const cors = require("cors");
 const app = express();
 app.use(cors({ origin: true }));
 
+const serviceAccount = require("./permissions.json");
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://fir-api-7aae9.firebaseio.com",
+});
+const db = admin.firestore();
 app.get("/hello-world", (req, res) => {
   return res.status(200).send("Hello World!");
+});
+// create
+app.post("/api/create", (req, res) => {
+  (async () => {
+    try {
+      await db
+        .collection("items")
+        .doc("/" + req.body.id + "/")
+        .create({ item: req.body.item });
+      return res.status(200).send();
+    } catch (error) {
+      console.log(error);
+      return res.status(500).send(error);
+    }
+  })();
 });
 
 exports.app = functions.https.onRequest(app);
